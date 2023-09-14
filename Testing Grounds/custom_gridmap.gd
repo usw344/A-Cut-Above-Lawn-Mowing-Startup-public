7eca52243e59a5d69c3bc5ee2be2b6c7cb77c886
usw344 <22089726+usw344@users.noreply.github.com>
2023-09-13 22:00:34 -0600

    adding custom gridmap implementation

--- Testing Grounds/custom_gridmap.gd
+++ Testing Grounds/custom_gridmap.gd
@@ -1,42 +1,37 @@
 extends Node3D
 
-@onready var gridmap:GridMap = $"Mowing Area/Grass Grid"
-
 var width_and_length:int = 200
 var total_grid_width_length:int # calcuated in ready function
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	
 	set_inital_positions_and_sizes()
 
-	make_inital_gridmap()
-	
-	#connect signal for collision
-	$"Small Gas Mower".collided.connect(handle_collision)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$"Small Gas Mower".position.y = 0
-#	print($"Small Gas Mower".position)
+	pass
 
-func make_inital_gridmap():
-	"""
-	Based on the given grid location
-	make the inital gridmap.
-	""" 
-	# for gridmap cell 2, the reference is 2m-> 1 grass
-	# 800m -> 400 distance
-	
-	for x in range(-total_grid_width_length, total_grid_width_length, 1):
-		for z in range(-total_grid_width_length, total_grid_width_length, 1):
-			gridmap.set_cell_item(Vector3i(x, 0, z), 6)
-	
-func update_gridmap():
-	"""
-	Based on the current gridmap update any cells that need to be updated
-	"""
+## custom gridmap variables
+var grid_length:int 
+var grid_width:int
 
+var batching_size:int # how many meshes togather in a multimesh
+
+# for the given widthxlength make a grid with a key represendting global coord of that item
+var grid_mapping:Dictionary = {} # key = 
+## custom gridmap api functions
+func set_grid_paramters(width:int, length:int, mesh_library:Mesh_Library,batching:int = 16):
+	grid_length = length
+	grid_width = width
+	batching_size = batching # default is 16
+
+func set_gridspace_item(item:Grass_Grid_Item):
+	"""
+	Set a item into gridmap. Provide a grass grid item with at least the position and mesh_id set
+	
+	CONDITION: mesh_id that is set should match what is the library_object
+	"""
 	pass
 
 func set_inital_positions_and_sizes():
@@ -61,18 +56,3 @@ func set_inital_positions_and_sizes():
 	
 	# set the mower to the start position
 	$"Small Gas Mower".position = $"Start Area".position
-
-func handle_collision(collision_array:Array):
-	for collision in collision_array:
-		if collision.get_collider().name != "Grass Grid":
-			continue
-
-		var position_of_collision:Vector3 = collision.get_position()
-		
-		var grid_position_of_collision:Vector3i = Vector3i(position_of_collision.x,0,position_of_collision.z)   
-
-		grid_position_of_collision = $"Mowing Area".to_local(grid_position_of_collision)
-
-
-		gridmap.set_cell_item(grid_position_of_collision,-1)
-
