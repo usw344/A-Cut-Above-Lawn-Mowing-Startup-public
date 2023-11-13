4a52c934ec2fb85f4b15706f8b4f6a71796d5193
usw344 <22089726+usw344@users.noreply.github.com>
2023-11-12 22:55:31 -0600

    adding in container for custom gridmap
    this is to add the UI while mowing

---
+++ Mowing Section/Mowing Area/Mowing Object/Mowing Object.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+"""
+Object to hold the actual mowing game. This contains the Custom_Gridmap and also
+the mowing UI. 
+
+This object has a save_object() and load_object() method
+
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
