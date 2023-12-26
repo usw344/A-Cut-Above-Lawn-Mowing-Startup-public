e6aab80ef6311e1f33e23d5875a6ff7b2b808b0d
Usman <22089726+usw344@users.noreply.github.com>
2023-12-25 21:12:43 -0600

    adding in main area scene

--- Game.gd
+++ Game.gd
@@ -5,24 +5,7 @@ var job_scene:PackedScene = load("res://Mowing Section/Job/job.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# for testing. remove this and replace with a job generator
-	var job_data = Job_Data_Container.new()
-	var type: String = "large"
-	var variant: int = 1
-	
-	job_data.init_default(1)
-	job_data.set_house_type(type)
-	job_data.set_house_variant(variant)
-
-	# set the size and scale for the job
-	var grass_info = model.get_grass_info(type)
-	job_data.set_house_scale(model.get_house_scale(type))
-	job_data.set_grass_size(grass_info["size"])
-	job_data.set_grass_scale(grass_info["scale"])
-
-	var job = job_scene.instantiate()
-	add_child(job)
-	job.set_data(job_data)
+	pass
 
 # this is for testing to toggle 
 func _process(delta):
---
+++ Main Area/Main Area.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+"""
+
+This is a abstraction layer that ties into whatever is selected as the background visual scene
+
+For now I am using a simple canvas layer with buttons as place holder
+
+Plans are that later to use a 3d scene with buildings and such
+
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
