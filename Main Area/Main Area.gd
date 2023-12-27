c3dc9689408078bb06f07a3c1dae5deff7c26f6c
Usman <22089726+usw344@users.noreply.github.com>
2023-12-26 20:29:09 -0600

    working on information bar for the main screen

--- Main Area/Main Area.gd
+++ Main Area/Main Area.gd
@@ -3,10 +3,13 @@ extends Node3D
 """
 
 This is a abstraction layer that ties into whatever is selected as the background visual scene
+This will take the UI based signals and wrap them up. So later if the UI changes this abstracion will remain and
+the Main scene which would have a reference this Main Area scene 
 
 For now I am using a simple canvas layer with buttons as place holder
 
-Plans are that later to use a 3d scene with buildings and such
+Plans are that later to use a 3d scene with buildings and such. 
+
 
 """
 
