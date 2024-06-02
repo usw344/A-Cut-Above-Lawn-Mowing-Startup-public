0c6cdffcc3e94df90914f036682bb764d65b3f50
Usman <22089726+usw344@users.noreply.github.com>
2024-06-01 23:30:45 -0600

    continued work on job manager to handle issue
    Issue being that when all job offers are declined. New offers that come later do not show up

--- Managers/Job manager/job_manager.gd
+++ Managers/Job manager/job_manager.gd
@@ -17,7 +17,7 @@ var job_generator:Job_Generator
 var job_offer_display:Job_Offer_Display
 var is_job_offer_display_visible:bool = false
 
-var job_offer_display_layer:CanvasLayer 
+@onready var job_offer_display_layer:CanvasLayer = $Job_Offer_Display_Layer
 
 var job_offer_display_scene = load("res://Managers/Job manager/Job Display/Job_offer_display.tscn")
 
@@ -32,7 +32,7 @@ func _ready():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$CanvasLayer/Label.text = str($"Job Generator".timer.get_wait_time())
 
 func recieve_job_offer(offer:Job_Offer):
 	"""
@@ -51,9 +51,22 @@ func recieve_job_offer(offer:Job_Offer):
 	NOTE: by this time the offer has already been added to the model
 	"""
 
-	add_child(offer)
-	offer.connect("remove_offer",remove_job_offer)
+
+	## TODO START
+	# until TODO END this block of code is for an edge case where once all offers have been declined
+	# for some reason the display does not update to show the new offers that come in after.
+	# I think this is an issue inside the << job offer display >> class and has to do with 
+	# what job_offer_display_id (some variable to that similar name) is set
+	# as a shortcut for now I am adding this new block which will check to see if this case is met and 
+	# will delete the old instance job_offer_display and make a new one
+	if(model.get_all_job_offers().size() == 0 and is_job_offer_display_visible):
+		hide_job_offer_display_menu() # remove the existing job offer display menu
+		show_job_offer_display_menu() # add new menu
 	
+	## TODO END
+	
+	add_child(offer) # so its timmer and stuff can work
+	offer.connect("remove_offer",remove_job_offer)
 	job_offer_display.update_display() # this will pull from the model
 
 
@@ -69,19 +82,15 @@ func remove_job_offer(offer:Job_Offer) -> void:
 			b. in case something else is needed. Do that.
 		4. Remove the Job Offer child from this object.
 		
-	NOTE: all job offers are a child of Job_Manager. So removing 
+	NOTE: all job offers are a child of Job_Manager. 
 	
 	"""
 	## remove from the model
 	model.remove_job_offer(offer)
 	
-	
-	# TODO: ADD CHECK TO SEE IF THE JOB_OFFER_DISPLAY IS STILL IN SCENE
-	
 	# update the display
 	job_offer_display.update_display()
-	
-	
+
 	# remove child node 
 	remove_child(offer)
 
@@ -101,20 +110,14 @@ func decline_job_offer(offer:Job_Offer):
 	remove_child(offer)
 
 
-func show_job_offer_display_menu():
+func show_job_offer_display_menu() -> void:
 	"""
 	When the signal is recieved to display the job offer menue this function will make a fresh
 	job_offer_display object and add it to the scene.
 	
 	"""
-	var return_objects:Array = make_new_job_offer_display()
-	var layer:CanvasLayer = return_objects[0]
+	job_offer_display = make_new_job_offer_display()
 	
-	
-	job_offer_display = return_objects[1] # reference to scene
-	job_offer_display_layer =layer # store this layer variable in a script variable
-	
-	add_child(job_offer_display_layer)
 	job_offer_display_layer.add_child(job_offer_display)
 	
 	# attach all relevent signals 
@@ -123,24 +126,21 @@ func show_job_offer_display_menu():
 	
 	is_job_offer_display_visible = true
 	
-func hide_job_offer_display_menu():
+func hide_job_offer_display_menu() ->void:
 	"""
 	When signal is recieved remove the menu from view (delete the object and it can be made again later)
 	"""
-	remove_child(job_offer_display_layer)
+	job_offer_display_layer.remove_child(job_offer_display)
 	
 	is_job_offer_display_visible = false
 
-func make_new_job_offer_display():
+func make_new_job_offer_display() -> Job_Offer_Display:
 	"""
 	Return a Job Offer Display object encapsulated in a Canvas layer object
 	
-	Return a two item array. One with the reference to the Canvas layer (which can be added to the scene)
-	
-	and the other is the direct reference to the Job Offer Display object
+	Return direct reference to the Job Offer Display object
 	"""
-	var canvas_layer:CanvasLayer = CanvasLayer.new()
 	var job_offer_display_object:Job_Offer_Display = job_offer_display_scene.instantiate()
 	
 	
-	return [canvas_layer,job_offer_display_object]
+	return job_offer_display_object
